2a052566d1a0dc45ee2d77296817b6f865ce2f0d
Dhruvik <dhruviksparikh@gmail.com>
2022-03-28 05:18:57 +0000

    Added changes

---
+++ src/main/java/com/simplilearn/Calculator.java
@@ -0,0 +1,10 @@
+package com.simplilearn;
+
+
+public class Calculator {
+
+	int calculate(int a, int b) {
+		return a+b;
+	}
+	
+}
---
+++ src/test/java/com/simplilearn/Demo7.java
@@ -0,0 +1,25 @@
+package com.simplilearn;
+
+import javax.xml.ws.Dispatch;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class Demo7 {
+
+	@Test
+	@Tag("my-tag")
+	void test(TestInfo testInfo, TestReporter testReporter) {
+		
+	String displayName =	testInfo.getDisplayName();
+	
+		System.out.println(displayName);
+		
+		testReporter.publishEntry(displayName);
+	}
+}
